f5450f31269ceffe8e2040ffd8def76a025d0c81
lucky <takuto.yoshikai@gmail.com>
2021-05-25 16:30:59 +0900

    certificates

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Header from "./Header";
 import Top from "./views/Top";
 import SignIn from "./views/SignIn";
 import Registration from "./views/Registration";
+import Certificates from "./views/Certificates";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Registration />
+      <Certificates />
     </div>
   );
 }
---
+++ src/views/Certificates.js
@@ -0,0 +1,44 @@
+
+function Certificates() {
+  return (
+    <div className="certificates">
+      <div className="certificates-content">
+        <p className="certificates-title">
+          証明書
+        </p>
+        <div className="certificates-list">
+          <div className="certificates-list-cell">
+            <img src="" className="certificates-list-cell-icon"/>
+            <div className="certificates-list-cell-detail">
+              <p className="certificates-list-cell-title">
+                STARTUP STUDIO MASTER 
+              </p>
+              <p className="certificates-list-cell-date">
+                January 30th, 2020
+              </p>
+              <p className="certificates-list-cell-by">
+                Gaiax Co Ltd.
+              </p>
+            </div>
+          </div>
+          <div className="certificates-list-cell">
+            <img src="" className="certificates-list-cell-icon"/>
+            <div className="certificates-list-cell-detail">
+              <p className="certificates-list-cell-title">
+                STARTUP STUDIO MASTER 
+              </p>
+              <p className="certificates-list-cell-date">
+                January 30th, 2020
+              </p>
+              <p className="certificates-list-cell-by">
+                Gaiax Co Ltd.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Certificates;
