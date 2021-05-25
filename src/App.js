b89186954473c10afe46543f9ae5ebc71b151d87
lucky <takuto.yoshikai@gmail.com>
2021-05-25 15:39:49 +0900

    top view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import Header from "./Header";
+import Top from "./views/Top";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Top />
     </div>
   );
 }
--- src/views/Top.js
+++ src/views/Top.js
@@ -1,9 +1,16 @@
 
 function Top() {
   return (
-    <div className="App">
-      <div className="top">
+    <div className="top">
+      <div className="top-left">
         
+      </div>
+      <div className="top-right">
+        <p className="concept">確かな証明書を誰にでも簡単に</p>
+        <p className="concept-large">ブロックチェーン技術による、改ざんされない確かな証明書を簡単に発行、受け取りすることができます。</p>
+        <div className="sign-up">
+          SIGN UP
+        </div>
       </div>
     </div>
   );
