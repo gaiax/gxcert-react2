fa68a9360f3cf46b0658e680dbb3e413b1a896a7
lucky <takuto.yoshikai@gmail.com>
2021-08-05 12:37:54 +0900

    props

--- src/App.js
+++ src/App.js
@@ -16,7 +16,16 @@ function App() {
       <Switch>
         <Route exact={true} path="/" component={Top} />
         <Route exact={true} path="/signup" component={SignIn} />
-        <Route exact={true} path="/new" component={NewCert} />
+        <Route exact={true} path="/new" render={
+          <NewCert
+            onChangeTitle={this.props.onChangeTitle}
+            onChangeDescription={this.props.onChangeDescription}
+            onChangeUrl={this.props.onChangeUrl}
+            onChangeImage={this.props.onChangeImage}
+            onChangeFrom={this.props.onChangeFrom}
+            onChangeTo={this.props.onChangeTo}
+          />
+        } />
       </Switch>
     </div>
   );
