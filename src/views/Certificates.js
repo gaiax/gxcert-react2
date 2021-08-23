082a391356f1ebaaf27253283fcea060d97126e9
takuto <takuto.yoshikai@gmail.com>
2021-08-23 17:36:11 +0900

    tmp certs

--- src/views/Certificates.js
+++ src/views/Certificates.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 function Certificates(props) {
   return (
@@ -10,20 +11,22 @@ function Certificates(props) {
           { props.certificates.length === 0 ? <p className="certificate-not-found">Certificate not found.</p> : "" }
           { props.certificates.map((certificate, index) => {
             return (
-              <div className="certificates-list-cell">
-                <img src={certificate.imageUrl} className="certificates-list-cell-icon"/>
-                <div className="certificates-list-cell-detail">
-                  <p className="certificates-list-cell-title">
-                    {certificate.title} 
-                  </p>
-                  <p className="certificates-list-cell-date">
-                    { certificate.issued_at ? (new Date(certificate.issued_at)).toISOString() : "" }
-                  </p>
-                  <p className="certificates-list-cell-by">
-                    {certificate.from}
-                  </p>
+              <Link to={"/certs/" + certificate.cid}>
+                <div className="certificates-list-cell">
+                  <img src={certificate.imageUrl} className="certificates-list-cell-icon"/>
+                  <div className="certificates-list-cell-detail">
+                    <p className="certificates-list-cell-title">
+                      {certificate.title} 
+                    </p>
+                    <p className="certificates-list-cell-date">
+                      { certificate.issued_at ? (new Date(certificate.issued_at)).toISOString() : "" }
+                    </p>
+                    <p className="certificates-list-cell-by">
+                      {certificate.from}
+                    </p>
+                  </div>
                 </div>
-              </div>
+              </Link>
             );
           }) };
         </div>
