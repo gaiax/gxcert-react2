25581db144579b2f86e6a24f7f8dfddc63086cbf
lucky <takuto.yoshikai@gmail.com>
2021-08-26 14:08:37 +0900

    change contract address

--- src/config.js
+++ src/config.js
@@ -3,14 +3,14 @@ let config;
 if (process.env.NODE_ENV === "development") {
   config = {
     web3Host: "https://matic-mumbai.chainstacklabs.com",
-    contractAddress: "0xB16070e105567515FCA925eD621229DDc75815B7",
+    contractAddress: "0x14B7c79b358Dd04c6c2E11a019FB84Ec3913a407",
     gxApi: "http://localhost:5001/gxcert-21233/asia-northeast1/gxcert",
   };
 } else if (process.env.NODE_ENV === "production") {
   //TODO: Change here
   config = {
     web3Host: "https://matic-mumbai.chainstacklabs.com",
-    contractAddress: "0xB16070e105567515FCA925eD621229DDc75815B7",
+    contractAddress: "0x14B7c79b358Dd04c6c2E11a019FB84Ec3913a407",
     gxApi: "http://localhost:5001/gxcert-21233/asia-northeast1/gxcert",
   }
 }
