7272fd7382a4533b2e114968f0a7804ccf7d1672
lucky <takuto.yoshikai@gmail.com>
2021-08-27 22:05:31 +0900

    decoration none

--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,8 @@ function Header(props) {
     <header className="App-header">
       <div className="header-wrapper">
         <Link to="/" className="logo">GxCert</Link>
+        <Link to="/certs" className="header-left-link">CREATED</Link>
+        <Link to="/new" className="header-left-link">ISSUE</Link>
       </div>
       <div className="sign-in-button" onClick={() => history.push("/signup")}>SIGN IN</div>
     </header>
