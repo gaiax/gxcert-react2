9b205c4e8cf639cb265844a976d4c3bddc3819de
lucky <takuto.yoshikai@gmail.com>
2021-08-27 21:57:54 +0900

    header

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,12 @@
 import history from "./history";
+import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
     <header className="App-header">
-      <div className="side-button">Side</div>
+      <div className="header-wrapper">
+        <Link to="/" className="logo">GxCert</Link>
+      </div>
       <div className="sign-in-button" onClick={() => history.push("/signup")}>SIGN IN</div>
     </header>
   );
