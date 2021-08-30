e7de1bd92fb891d204501b5b17163a783d26cf03
lucky <takuto.yoshikai@gmail.com>
2021-08-30 16:19:37 +0900

    change firebase endpoint

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === "development") {
   config = {
     web3Host: "https://matic-mumbai.chainstacklabs.com",
     contractAddress: "0xF9322C8f678244e5391B1B6c7aB32E1d5d3857A3",
-    gxApi: "https://asia-northeast1-gxcert-21233.cloudfunctions.net/gxcert",
+    gxApi: "http://localhost:5001/gxcert-21233/asia-northeast1/gxcert",
   };
 } else if (process.env.NODE_ENV === "production") {
   //TODO: Change here
