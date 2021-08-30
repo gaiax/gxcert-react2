656af533aa21dbc6e6aa1874c472ff6e45d13b4d
lucky <takuto.yoshikai@gmail.com>
2021-08-30 16:18:31 +0900

    gh-pages

--- src/config.js
+++ src/config.js
@@ -4,14 +4,14 @@ if (process.env.NODE_ENV === "development") {
   config = {
     web3Host: "https://matic-mumbai.chainstacklabs.com",
     contractAddress: "0xF9322C8f678244e5391B1B6c7aB32E1d5d3857A3",
-    gxApi: "http://localhost:5001/gxcert-21233/asia-northeast1/gxcert",
+    gxApi: "https://asia-northeast1-gxcert-21233.cloudfunctions.net/gxcert",
   };
 } else if (process.env.NODE_ENV === "production") {
   //TODO: Change here
   config = {
     web3Host: "https://matic-mumbai.chainstacklabs.com",
     contractAddress: "0xF9322C8f678244e5391B1B6c7aB32E1d5d3857A3",
-    gxApi: "http://localhost:5001/gxcert-21233/asia-northeast1/gxcert",
+    gxApi: "https://asia-northeast1-gxcert-21233.cloudfunctions.net/gxcert",
   }
 }
 
