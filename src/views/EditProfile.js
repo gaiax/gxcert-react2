2ed16d33c5e962a42518f7de7c1359240316ee37
lucky <takuto.yoshikai@gmail.com>
2021-09-07 11:59:35 +0900

    edit profile

---
+++ src/views/EditProfile.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { createImageUrlFromUint8Array } from "../util/ipfs";
+
+class EditProfile extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    let imageUrl = "";
+    try {
+      imageUrl = createImageUrlFromUint8Array(this.props.image);
+    } catch(err) {
+      console.error(err);
+    }
+    return (
+      <div className="edit-profile">
+        <div className="edit-profile-content">
+          <p className="edit-profile-title">
+            プロフィール編集
+          </p>
+          <div className="edit-profile-form">
+            <div className="edit-profile-form-image">
+              <label for="edit-profile-form-image-file">
+                <img src={imageUrl} className="edit-profile-form-image" />
+              </label>
+              <input id="edit-profile-form-image-file" type="file" onChange={this.props.onChangeProfileImage} />
+            </div>
+            <p className="edit-profile-form-title">Name</p>
+            <input type="text" className="edit-profile-form-name" onChange={this.props.onChangeProfileName} />
+            <p className="edit-profile-form-title">E-mail</p>
+            <input type="text" className="edit-profile-form-email" onChange={this.props.onChangeProfileEmail} />
+            <div className="register-button" onClick={this.props.registerProfile} >
+              更新
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EditProfile;
