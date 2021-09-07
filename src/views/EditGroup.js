4ca2caebaac1c668732dc7df1c9f666385c07806
lucky <takuto.yoshikai@gmail.com>
2021-09-07 12:03:47 +0900

    edit group

---
+++ src/views/EditGroup.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class EditGroup extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    if (!this.props.group) {
+      return (
+        <div className="edit-group">
+        </div>
+      );
+    }
+    return (
+      <div className="edit-group">
+        <div className="edit-group-content">
+          <p className="edit-group-title">発行元 {this.props.group.name}の更新</p>
+          <p className="edit-group-description">証明書の発行には発行元となる団体（企業、教育機関、NPO法人など）の登録が必要です。</p>
+          <div className="edit-group-form">
+            <p className="edit-group-form-title">Name</p>
+            <input type="text" className="edit-group-form-name" onChange={this.props.onChangeGroupName} />
+            <p className="edit-group-form-title">Address</p>
+            <input type="text" className="edit-group-form-address" onChange={this.props.onChangeGroupAddress} />
+            <p className="edit-group-form-title">Phone</p>
+            <input type="text" className="edit-group-form-phone" onChange={this.props.onChangeGroupPhone} />
+
+            <div className="register-button" onClick={this.props.registerGroup} >
+              登録
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EditGroup;
