642f1f21c3734fd87685aae5941d79cc5431421e
lucky <takuto.yoshikai@gmail.com>
2021-09-07 12:58:10 +0900

    session storage

--- src/store.js
+++ src/store.js
@@ -1,18 +1,26 @@
 import { combineReducers, createStore, compose, applyMiddleware } from "redux";
 import Reducer from "./reducer";
 import thunk from "redux-thunk";
+import { persistReducer, persistStore } from 'redux-persist'
+import sessionStorage from 'redux-persist/lib/storage/session'
 import initialState from "./initialState";
 
+const persistConfig = {
+    key: "root",
+    storage: sessionStorage,
+}
 
+const persistedReducer = persistReducer(persistConfig, Reducer);
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  combineReducers({
-    state: Reducer,
-  }),
-  initialState,
-  storeEnhancers(applyMiddleware(thunk))
+    combineReducers({
+          state: persistedReducer,
+        }),
+    initialState,
+    storeEnhancers(applyMiddleware(thunk))
 );
 
+export const persistor = persistStore(store);
 export default store;
