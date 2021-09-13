e41f60bbeeb312adb4fbf317e4e7938ebf2049c6
lucky <takuto.yoshikai@gmail.com>
2021-09-13 13:36:47 +0900

    image export

--- src/views/Certificate.js
+++ src/views/Certificate.js
@@ -3,6 +3,7 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 import Loader from "react-loader-spinner";
+import domtoimage from "dom-to-image";
 
 
 class Certificate extends React.Component {
@@ -17,10 +18,49 @@ class Certificate extends React.Component {
     console.log(this.props.userCert);
     return (
       <div className="certificate">
+        { (this.props.userCert && this.props.userCert.certificate) ? (
+        <div className="hidden">
+          <div className="certificate-content-export" id="certificate-content-export">
+            <p className="certificate-title">
+              {this.props.userCert.certificate.title}
+            </p>
+            <img src={this.props.certificateImage} className="certificate-icon" />
+            <table className="certificate-detail">
+              <tr>
+                <td>Issue Date: </td>
+                <td>{(new Date(parseInt(this.props.userCert.timestamp * 1000))).toISOString()}</td>
+              </tr>
+              <tr>
+                <td>Issuer: </td>
+                <td>
+                  {!this.props.userCert.certificate.group ? "" : this.props.userCert.certificate.group.name }
+                </td>
+              </tr>
+              <tr>
+                <td>To: </td>
+                <td>
+                  { this.props.userCert.to }
+                </td>
+              </tr>
+              <tr>
+                <td>Title: </td>
+                <td>
+                  {this.props.userCert.certificate.title}
+                </td>
+              </tr>
+              <tr>
+                <td>Description: </td>
+                <td>{this.props.userCert.certificate.description}</td>
+              </tr>
+            </table>
+            <a id="cert-link" download="cert.png" href="" ></a>
+          </div>
+        </div>
+        ) : ""}
         { (!this.props.userCert || !this.props.userCert.certificate) ? (
             <Loader type="Puff" color="#00BFFF" height={100} width={100} />
           ) : (
-            <div className="certificate-content">
+            <div className="certificate-content" id="certificate-content">
               <p className="certificate-title">
                 {this.props.userCert.certificate.title}
               </p>
@@ -53,11 +93,20 @@ class Certificate extends React.Component {
                   <td>{this.props.userCert.certificate.description}</td>
                 </tr>
               </table>
+              <a id="cert-link" download="cert.png" href="" ></a>
               <div className="certificate-buttons">
                 <div className="certificate-button">
                   参照URLの発行
                 </div>
-                <div className="certificate-button">
+                <div className="certificate-button" onClick={() => {
+                  domtoimage.toPng(document.getElementById("certificate-content-export"))
+                    .then(dataUrl => {
+                      document.getElementById("cert-link").href = dataUrl;
+                      document.getElementById("cert-link").click();
+                    }).catch(err => {
+                      
+                    });
+                }}>
                   PDFのダウンロード
                 </div>
               </div>
