e5d638f03abd3d3b1050c243a1f9b87562330b7a
Masahiro Kikuchi <masahiro.kikuchi@gaiax.com>
2021-09-14 11:29:06 +0900

    ヘッダーの文言修正

--- src/Header.js
+++ src/Header.js
@@ -7,19 +7,19 @@ function Header(props) {
       <div className="header-wrapper">
         <Link to="/top" className="logo">GxCert</Link>
         { props.isLoggedIn ? (
-            <Link to="/" className="header-left-link">OBTAINED</Link>
+            <Link to="/" className="header-left-link">取得証明書</Link>
           ) : ""
         }
         { props.isLoggedIn ? (
-          <Link to="/issue" className="header-left-link">ISSUE</Link>
+          <Link to="/issue" className="header-left-link">証明書発行</Link>
           ) : ""
         }
       </div>
       { !props.isLoggedIn ? (
-          <div className="sign-in-button" onClick={() => history.push("/signup")}>SIGN IN</div>) : (
+          <div className="sign-in-button" onClick={() => history.push("/signup")}>Sign in</div>) : (
           <div className="header-right-links">
-            <Link to="/profile/edit" className="edit-profile-button">PROFILE</Link>
-            <div className="sign-out-button" onClick={props.signOut}>SIGN OUT</div>
+            <Link to="/profile/edit" className="edit-profile-button">プロフィール</Link>
+            <div className="sign-out-button" onClick={props.signOut}>Sign out</div>
           </div>
           )
       }
